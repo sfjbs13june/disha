e0feee33d359864582309b68b65f5d28cbe0b605
sfjbs <dg.wadhe@tcs.com>
2023-01-12 07:29:49 +0000

    java second project

---
+++ day01/second-java-project/src/main/java/com/disha/app/Greeting.java
@@ -0,0 +1,16 @@
+package com.disha.app;
+
+public class Greeting {
+
+    public String SayWelcome() {
+        return "welcome to greeting";
+    }
+    public String SayHello() {
+        return "hello to greeting";
+    }
+    public String SayBye() {
+        return "bye to greeting";
+    }
+
+    }
+
