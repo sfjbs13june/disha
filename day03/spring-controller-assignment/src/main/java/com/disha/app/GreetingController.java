038bc83fda408e229392bdbe45ee50dc68ec366b
sfjbs <dg.wadhe@tcs.com>
2023-01-16 19:00:23 +0000

    greeting controller

---
+++ day03/spring-controller-assignment/src/main/java/com/disha/app/GreetingController.java
@@ -0,0 +1,29 @@
+package com.disha.app;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/greet")
+    public String Greet() {
+        return "Greet from App";
+    }
+
+    @GetMapping("/welcome")
+    public String Welcome(){
+        return "Welcome from App";
+
+    }
+    @GetMapping("/hi")
+    public String Hi(){
+        return "Hi from App";
+
+    }
+    @GetMapping("/thanks")
+    public String Thanks(){
+        return "Thanks from App";
+
+    }
+
+}
