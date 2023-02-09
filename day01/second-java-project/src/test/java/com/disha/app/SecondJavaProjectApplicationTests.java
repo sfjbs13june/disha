e19c0ff39a5cb6403518b8947df5a50047598f6d
sfjbs <wadhedishu2000@gmail.com>
2023-02-09 10:18:16 +0000

    day 01

---
+++ day01/second-java-project/src/test/java/com/disha/app/SecondJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disha.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
